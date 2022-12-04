1789159d78ad4e8b8be771745b02cb161a70248d
Ekaksh Janweja <jekaksh@gmail.com>
2022-12-04 14:48:08 +0530

    What are view text their style and react functions

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello World!!</Text>
     </View>
-  )
+  );
 }
